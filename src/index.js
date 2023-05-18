b3b27575b9f767fc29bafd34b4ffc0f6539a3e58
Damien Voindrot <damien.voindrot@link-value.fr>
2023-05-18 13:11:27 +0200

    404 work

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 import { React } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-/* import { createBrowserRouter, RouterProvider } from "react-router-dom"; */
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import { store } from "./store/redux";
 
 import "./styles/css/index.css";
 
 import App from "./App";
 import Home from "./pages/HomePage/HomePage";
+import Page404 from "./pages/Page404";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 
-/* const router = createBrowserRouter([
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Navigate replace to="/" />,
+  },
   {
     path: "/",
     element: <App page={<Home />} />,
   },
-]); */
+]);
 
 root.render(
   <Provider store={store}>
-    <App page={<Home />} />
+    <RouterProvider router={router} />
   </Provider>,
 );
---
+++ src/pages/Page404.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Page404() {
+  return <p>404 ERRRROR</p>;
+}
+
+/* Page404.defaultProps = {
+  position: undefined,
+};
+
+Page404.propTypes = {
+  position: PropTypes.string,
+};
+ */
+
+export default Page404;
