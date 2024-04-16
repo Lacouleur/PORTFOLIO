b6b34b249b12e61cfd717e90ec4ea438b522bb8a
Damien Voindrot <damien.voindrot@link-value.fr>
2024-04-16 15:59:08 +0200

    Updates

--- src/utils/helpers/appHelper.js
+++
@@ -1,8 +0,0 @@
-export function lockScrollBody(isFullView) {
-  const body = document.getElementById("body");
-  if (isFullView.toogle) {
-    body.setAttribute("style", "overflow: hidden;");
-  } else {
-    body.setAttribute("style", "overflow: visible;");
-  }
-}
